bdda33ba76eae329557abdddb9a74ba1c92f006d
CW-Logothetis <cwlogo@pm.me>
2021-12-22 04:04:02 +0200

    Photo gallery added to mobile

--- script.js
+++ script.js
@@ -40,3 +40,38 @@ openButton.addEventListener("click", () => {
   }
   // as the above turns off display of all tabs, this sets History to load
   document.getElementById("default-open").click();
+
+// -----------------------------------------------------------------------------
+// -------------                Photo Gallery           ------------------------
+// -----------------------------------------------------------------------------
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+ 
+  let slides = document.getElementsByClassName("photographs");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
