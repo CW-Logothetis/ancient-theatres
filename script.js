83ce759dd8353e8e29babd26f805fc5609a6f119
CW-Logothetis <cwlogo@pm.me>
2021-12-30 01:12:36 +0200

    update gallery id thumbnail

--- script.js
+++ script.js
@@ -61,17 +61,17 @@ function currentSlide(n) {
 function showSlides(n) {
  
   let slides = document.getElementsByClassName("photographs");
-  let dots = document.getElementsByClassName("demo");
+  let thumb = document.getElementsByClassName("thumbnail");
   let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < thumb.length; i++) {
+    thumb[i].className = thumb[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  thumb[slideIndex-1].className += " active";
+  captionText.innerHTML = thumb[slideIndex-1].alt;
 }
